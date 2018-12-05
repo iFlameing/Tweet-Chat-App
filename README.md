8dd443bb955773fe8fa7ae3bac3f4ebd66766e06
iFlameing <ialokkumarsingh0@gamil.com>
2018-12-05 20:11:39 +0530

    updating the readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-<h1>This is simple project madeup using node express as backend and client as React and connecting them by react proxy method </h1>
+<h1>Tweet-Chat-App</h1>
+<p>This is a simple tweet-chat-app madeup using node.js express as backend and react,redux as frontend .</p>
+
+<h3>How to build the project</h3>
+<ul>
+<li>Clone this repo</li>
+<li>Install mongodb by going to this link.</li>
+<li>Always restart the mongodb by runing this command 'sudo service mongodb start'</li>
+<li>Then run npm install </li>
+<li>Then go into tweet-app-server file and run the command npm start</li>
+<li>Then go to tweet-client and open another terminal and then type command npm start </li>
+<p>Thats It</p>
+<p>YOu see the app running in the default browser at port 3000</p>
+
+</ul>
--- tweet-client/src/container/message/message.js
+++ tweet-client/src/container/message/message.js
@@ -1,62 +1,65 @@
-import React,{Component} from 'react'
-import {connect} from 'react-redux'
-import {message} from '../../action/index'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { message } from '../../action/index'
 import MessageLoader from '../messages/messages'
 import classes from './message.css'
 import Aux from '../../hoc/aux'
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import Profile from '../../components/ProfileSidebar/profile'
 
 
-class Message extends Component{
+class Message extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.loadmessage();
     }
-    render(){
-        const message = this.props.messages.reverse().filter((v,i)=> {
-                        return i<10;
+    render() {
+        const message = this.props.messages.reverse().filter((v, i) => {
+            return i < 10;
         })
         console.log(message);
         let user = JSON.parse(localStorage.getItem('user-for-tweetApp'))
-        console.log(user.username);
-        let image= `http://localhost:8081/${user.profileImageUrl}`
-        let message1="";
-        let count=0;
-        if(message.length!==0){
-            count++;
-         message1 =  message.map(mes=>( <div key={count.toString()} ><MessageLoader   name={mes.userId.username} text={mes.text} postimage={`http://localhost:8081/${mes.image}`} img={`http://localhost:8081/${mes.userId.profileImageUrl}`} /></div>))
+        let image = ''
+        if (user) {
+            image = `http://localhost:8081/${user.profileImageUrl}`
         }
-    
-        return(
+        let message1 = "";
+        let count = 0;
+        if (message.length !== 0) {
+            count++;
+            message1 = message.map(mes => (<div key={count.toString()} ><MessageLoader name={mes.userId.username} text={mes.text} postimage={`http://localhost:8081/${mes.image}`} img={`http://localhost:8081/${mes.userId.profileImageUrl}`} /></div>))
+        }
+
+        return (
             <div className={classes.container}>
                 <Aux>
                     <div className={classes.first}>
-                    <Profile image={image} username={this.props.username}/></div>
+                        <Profile image={image} username={this.props.username} /></div>
                     <div className={classes.message} >
-                    {message1}
+                        {message1}
                     </div>
                     <div className={classes.second}>
-                    <h1>Who To Follow <span className={classes.span}><NavLink to="/">Refresh</NavLink></span> <span className={classes.span}><NavLink to="/">ViewAll</NavLink></span></h1>
-                    <p>This website is madeup by Alok Kumar just for the learning purpose and how
-                        the new technology is evolving . I have to implement more and more feature so that 
-                        I learn the new technology very fast.
+                        <h1>Who To Follow <span className={classes.span}><NavLink to="/">Refresh</NavLink></span> <span className={classes.span}><NavLink to="/">ViewAll</NavLink></span></h1>
+                        <p>This website is madeup by Alok Kumar just for the learning purpose and how
+                            the new technology is evolving . I have to implement more and more feature so that
+                            I learn the new technology very fast.
                     </p>
                     </div>
                 </Aux>
             </div>
-        )}
-}
-
-const mapStateToProps = state=>({
-    messages:state.message.message,
-    username:state.losin.userName
-})
-
-const mapDispatchToProps =(dispatch)=>{
-    return{
-        loadmessage:()=>dispatch(message()),   
+        )
     }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Message);
+const mapStateToProps = state => ({
+    messages: state.message.message,
+    username: state.losin.userName
+})
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loadmessage: () => dispatch(message()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Message);
